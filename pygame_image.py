aba69f4315c64914b68bd5acf8e130d030a81766
tyaku <c0b2315233@edu.teu.ac.jp>
2024-04-16 16:32:50 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -30,9 +30,10 @@ def main():
         elif key_lst[pg.K_DOWN]:
             kt_rct.move_ip( 0, 1 )
         elif key_lst[pg.K_RIGHT]:
-            kt_rct.move_ip( 1, 0 )
+            kt_rct.move_ip( 2, 0 )
         elif key_lst[pg.K_LEFT]:
             kt_rct.move_ip( -1 , 0 )
+        kt_rct.move_ip( -1 , 0 )
         screen.blit(bg_img, [bg_x, 0])              #背景の場所設定
         screen.blit(bg_img2,[bg_x+1600,0])          #反転した背景の場所設定
         screen.blit(bg_img,[bg_x+3200,0])           #3枚目の背景の場所設定

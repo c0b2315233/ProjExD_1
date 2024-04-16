befebff7442386b5db319393ebc4f2b3c0bd667b
tyaku <c0b2315233@edu.teu.ac.jp>
2024-04-16 16:12:03 +0900

    Q8-2

--- pygame_image.py
+++ pygame_image.py
@@ -14,17 +14,20 @@ def main():
     bg_x = 0
 
     kt_img = pg.image.load("fig/3.png")             #こうかとんの画像のロード
-    kt_img =pg.transform.flip(kt_img,True,False)    #画像の反転
+    kt_img = pg.transform.flip(kt_img,True,False)    #画像の反転
+    kt_rct = kt_img.get_rect()                      #こうかとんのrectを設定
+    kt_rct.center=300,200                           #中心点を初期位置に設定
+    
 
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return        
-
+        
         screen.blit(bg_img, [bg_x, 0])              #背景の場所設定
-        screen.blit(bg_img2,[bg_x+1600,0])
-        screen.blit(bg_img,[bg_x+3200,0])
-        screen.blit(kt_img,[300,200])               #こうかとんの場所指定
+        screen.blit(bg_img2,[bg_x+1600,0])          #反転した背景の場所設定
+        screen.blit(bg_img,[bg_x+3200,0])           #3枚目の背景の場所設定
+        screen.blit(kt_img,kt_rct)                  #こうかとんの場所指定
         pg.display.update()                         #表示
         print(bg_x)
         bg_x -= 1                                   #背景のX座標を1ずつ増加

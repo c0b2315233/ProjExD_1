6cb82904a92eed51161d2c84ffe55261317632a4
tyaku <c0b2315233@edu.teu.ac.jp>
2024-04-16 17:19:34 +0900

    add comments and remove test codes

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -23,25 +23,31 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return        
-        kt_diff_x=0                                 
-        kt_diff_y=0
+        kt_diff_x=0                                 #こうかとんの移動状態を記憶する変数を用意
+        kt_diff_y=0                                 
+        
         key_lst=pg.key.get_pressed()                #全キーの押下状態を確認する
         if key_lst[pg.K_UP]:                        #上キー押下
-            kt_diff_y=-10                            #
+            kt_diff_y=-1                            #Y方向の変数を-1する
+        
         elif key_lst[pg.K_DOWN]:                    #下キー押下
-            kt_diff_y=10
+            kt_diff_y=1                             #Y方向の変数を+1する
+        
         elif key_lst[pg.K_RIGHT]:                   #右キー押下
-            kt_diff_x=3
+            kt_diff_x=3                             #X方向の変数を+3する
+        
         elif key_lst[pg.K_LEFT]:                    #左キー押下
-            kt_diff_x=-1
-        kt_rct.move_ip( -1+kt_diff_x , kt_diff_y )
+            kt_diff_x=-1                            #X方向の変数を-1する
+        
+        kt_rct.move_ip( -1+kt_diff_x , kt_diff_y )  #変数に合わせて移動させる
+
         screen.blit(bg_img, [bg_x, 0])              #背景の場所設定
         screen.blit(bg_img2,[bg_x+1600,0])          #反転した背景の場所設定
         screen.blit(bg_img,[bg_x+3200,0])           #3枚目の背景の場所設定
         screen.blit(kt_img,kt_rct)                  #こうかとんの場所指定
         pg.display.update()                         #表示
-        print(bg_x)
-        bg_x -= 1                                   #背景のX座標を1ずつ増加
+        
+        bg_x -= 1                                   #背景のX座標を1ずつ減少
         if(bg_x<=-3200):                             #背景のX座標が1600以上の場合
             bg_x=0                                  #背景のX座標をリセットする
         tmr += 1                                    

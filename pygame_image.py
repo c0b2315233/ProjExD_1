7107416b5cf9f3220b3f2c42e199501ce5fa0bdd
tyaku <c0b2315233@edu.teu.ac.jp>
2024-04-16 16:46:34 +0900

    演習2

--- pygame_image.py
+++ pygame_image.py
@@ -10,30 +10,31 @@ def main():
     screen = pg.display.set_mode((800, 600))        #サイズを(800,600)にする
     clock  = pg.time.Clock()                        
     bg_img = pg.image.load("fig/pg_bg.jpg")         #背景画像の選択
-    bg_img2 = pg.transform.flip(bg_img,True,False)
-    bg_x = 0
+    bg_img2 = pg.transform.flip(bg_img,True,False)  #反転処理
+    bg_x = 0                                        #x座標のカウンタを用意
 
     kt_img = pg.image.load("fig/3.png")             #こうかとんの画像のロード
     kt_img = pg.transform.flip(kt_img,True,False)    #画像の反転
     kt_rct = kt_img.get_rect()                      #こうかとんのrectを設定
     kt_rct.center=300,200                           #中心点を初期位置に設定
     
-
     tmr = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return        
-        
-        key_lst=pg.key.get_pressed()
-        if key_lst[pg.K_UP]:
-            kt_rct.move_ip( 0, -1 )
-        elif key_lst[pg.K_DOWN]:
-            kt_rct.move_ip( 0, 1 )
-        elif key_lst[pg.K_RIGHT]:
-            kt_rct.move_ip( 2, 0 )
-        elif key_lst[pg.K_LEFT]:
-            kt_rct.move_ip( -1 , 0 )
-        kt_rct.move_ip( -1 , 0 )
+        kt_diff_x=0                                 
+        kt_diff_y=0
+        key_lst=pg.key.get_pressed()                #全キーの押下状態を確認する
+        if key_lst[pg.K_UP]:                        #上キー押下
+            kt_diff_y=-10                            #
+        elif key_lst[pg.K_DOWN]:                    #下キー押下
+            kt_diff_y=10
+        elif key_lst[pg.K_RIGHT]:                   #右キー押下
+            kt_diff_x=3
+        elif key_lst[pg.K_LEFT]:                    #左キー押下
+            kt_diff_x=-1
+        kt_rct.move_ip( -1+kt_diff_x , kt_diff_y )
         screen.blit(bg_img, [bg_x, 0])              #背景の場所設定
         screen.blit(bg_img2,[bg_x+1600,0])          #反転した背景の場所設定
         screen.blit(bg_img,[bg_x+3200,0])           #3枚目の背景の場所設定

a150b73df3737047eae3a57605a0322d41c71dfc
tyaku <c0b2315233@edu.teu.ac.jp>
2024-04-16 15:07:23 +0900

    Q5

--- pygame_image.py
+++ pygame_image.py
@@ -7,23 +7,23 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((800, 600))
-    clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    screen = pg.display.set_mode((800, 600))        #サイズを(800,600)にする
+    clock  = pg.time.Clock()                        
+    bg_img = pg.image.load("fig/pg_bg.jpg")         #背景画像の選択
 
-    kt_img = pg.image.load("fig/3.png")
-    kt_img =pg.transform.flip(kt_img,True,False)
+    kt_img = pg.image.load("fig/3.png")             #こうかとんの画像のロード
+    kt_img =pg.transform.flip(kt_img,True,False)    #画像の反転
 
     tmr = 0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
+            if event.type == pg.QUIT: return        
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kt_img,[300,200])
-        pg.display.update()
+        screen.blit(bg_img, [0, 0])                 #背景の場所設定
+        screen.blit(kt_img,[300,200])               #こうかとんの場所指定
+        pg.display.update()                         #表示
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
